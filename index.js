6b6e67b99675749a344895ef96a59bb26e5442e7
rafaax <rafavfx1@gmail.com>
2023-08-03 13:13:38 -0300

    css botao e js

--- index.js
+++ index.js
@@ -1,71 +1,85 @@
 let url = 'fetch.php'
 let presencial;
+let isFetching = false;
+var myChart;
 
-function geraGrafico(data, data2){
+
+function geraGrafico(data, data2, func){
     const ctx = document.getElementById('myChart');
-
-    data = [
-        parseInt(data.presencial),  parseInt(data.remoto) , parseInt(data.compromisso_pessoal)
-    ];
-    data2 = [
-        parseInt(data2.presencial),parseInt(data2.remoto), parseInt(data2.compromisso_pessoal)
-    ];
-
-    let delayed;
+    if(data != null  && data2 != null){
+        data = [
+            parseInt(data.presencial),  parseInt(data.remoto) , parseInt(data.compromisso_pessoal)
+        ];
+        data2 = [
+            parseInt(data2.presencial),parseInt(data2.remoto), parseInt(data2.compromisso_pessoal)
+        ];
+    }
     
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Presencial', 'Remoto', 'Compromisso Pessoal'],
-            datasets: [{
-                label: 'Menor que meio dia',
-                data: data,
-                backgroundColor: [
-                    'rgb(255, 255, 255)'
-                ],
-                    borderColor: [
-                    'rgb(0, 0, 0)'
-                ],
-                borderWidth: 3
-            },
-            {
-                label: 'Maior que meio dia',
-                data: data2,
-                backgroundColor: [
-                    'rgb(202, 202, 202)'
-                ],
-                    borderColor: [
-                    'rgb(0, 0, 0)'
-                ],
-                borderWidth: 3
-            }
-            ]
-        },
-        options: {
-            animation: {
-                onComplete: () => {
-                    delayed = true;
-                },
-                delay: (context) => {
-                    let delay = 0;
-                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
-                    delay = context.dataIndex * 300 + context.datasetIndex * 100;
-                    }
-                    return delay;
-                },
-            },
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top',
-                },
-                title: {
-                    display: true,
-                    text: 'Reuniões Engeline'
-                }
-            }   
+    let delayed;
+
+    if(func == true){
+        let chartStatus = Chart.getChart("myChart"); // <canvas> id
+        if (chartStatus != undefined) {
+            document.querySelector("#chart").innerHTML = '<canvas id="myChart"></canvas>'; //recria o canvas
         }
-    });
+        // console.log(chartStatus);
+    }else{
+
+        myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Presencial', 'Remoto', 'Compromisso Pessoal'],
+                datasets: [{
+                    label: 'Menor que meio dia',
+                    data: data,
+                    backgroundColor: [
+                        'rgb(255, 255, 255)'
+                    ],
+                        borderColor: [
+                        'rgb(0, 0, 0)'
+                    ],
+                    borderWidth: 3
+                },
+                {
+                    label: 'Maior que meio dia',
+                    data: data2,
+                    backgroundColor: [
+                        'rgb(202, 202, 202)'
+                    ],
+                        borderColor: [
+                        'rgb(0, 0, 0)'
+                    ],
+                    borderWidth: 3
+                }
+                ]
+            },
+            options: {
+                animation: {
+                    onComplete: () => {
+                        delayed = true;
+                    },
+                    delay: (context) => {
+                        let delay = 0;
+                        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                        delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                        }
+                        return delay;
+                    },
+                },
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Reuniões Engeline'
+                    }
+                }   
+            }
+        });
+    }
+
 }
 
 async function getPhp(){
@@ -82,7 +96,7 @@ async function getPhp(){
             // console.log(response);
             const data1 = response.count1;
             const data2 = response.count2;
-            geraGrafico(data1, data2);
+            geraGrafico(data1, data2, false);
 
             return response;
         });
@@ -90,8 +104,32 @@ async function getPhp(){
     catch (error) {
         console.error("Erro na requisição:", error);
     }
-   
 }
 
+function quebraGrafico(atuador){
+    geraGrafico(null, null, atuador)
+}
 
-getPhp();
+const divElement = document.querySelector('div');
+const toggleButton = document.getElementById('grafico_barra');
+
+
+
+toggleButton.addEventListener('click', function() {
+  if (divElement.style.display === 'none') {
+    if (!isFetching) {
+        isFetching = true;
+        getPhp().then(() => {
+                isFetching = false;
+                // quebraGrafico();
+            }
+        );
+        
+    divElement.style.display = 'block';
+    
+  }} else {
+    divElement.style.display = 'none';
+    quebraGrafico(true);
+  }
+});
+
--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <!DOCTYPE html>
+<style>
+.button {
+        width: 220px;
+        height: 50px;
+        border: none;
+        outline: none;
+        color: #fff;
+        background: #111;
+        cursor: pointer;
+        position: relative;
+        border-radius: 10px;
+    }
+    canvas{
+        width:1000px !important;
+        height:600px !important;
+    }
+</style>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<div>
-  <canvas id="myChart" style="width:20%; height: 30%;"></canvas>
+<button id="grafico_barra" class="button" type="button">Gráfico de Barra</button>
+<div id="chart" style="display:none">
+  <canvas id="myChart"></canvas>
 </div>
 <script src="index.js"></script>
